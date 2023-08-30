61b3082b79562425165a0ccbb5a172a35ad8f87e
tokaretin <tokaretin@gmail.com>
2023-08-30 14:59:42 +0200

    task

--- Skillbox/tempCodeRunnerFile.cpp
+++ Skillbox/tempCodeRunnerFile.cpp
@@ -1,2 +1,33 @@
-for (;;)
-  {
+#include <iostream>
+// #include <string>
+
+// enum note 
+// {
+//     C = 1;
+//     D = 2;
+//     E = 4;
+//     F = 8;
+//     G = 16;
+//     A = 32;
+//     H = 64;
+// };
+
+// int main()
+// {
+//     int melody[12];
+
+//     for (int i = 0; i < 12; i++)
+//     {
+//         std::cout << "Введите комбинацию нот для аккорда: " << i + 1 << ": ";
+//         std::cin >> melody[i];
+//     }
+
+//     // Воспроизведение мелодии
+//     for (int i = 0; i < 12; i++)
+//     {
+//         std::cout << i + 1 << "Аккоррд" << ": ";
+//         if(melody[i] & C) std::cout << "C ";
+//     }
+    
+//     return 0;
+// }
