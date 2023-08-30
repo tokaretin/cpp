94e1c3a7d9a8b95b93a279b24a4a00af759776fa
tokaretin <tokaretin@gmail.com>
2023-08-30 20:38:26 +0200

    task

--- Skillbox/tempCodeRunnerFile.cpp
+++ Skillbox/tempCodeRunnerFile.cpp
@@ -1,33 +1,15 @@
-#include <iostream>
-// #include <string>
+ // for (int j = 0; j < maxCombination; j++)
+        // {
+        //     int noteImput;
+        //     std::cin >> noteImput;
 
-// enum note 
-// {
-//     C = 1;
-//     D = 2;
-//     E = 4;
-//     F = 8;
-//     G = 16;
-//     A = 32;
-//     H = 64;
-// };
-
-// int main()
-// {
-//     int melody[12];
-
-//     for (int i = 0; i < 12; i++)
-//     {
-//         std::cout << "Введите комбинацию нот для аккорда: " << i + 1 << ": ";
-//         std::cin >> melody[i];
-//     }
-
-//     // Воспроизведение мелодии
-//     for (int i = 0; i < 12; i++)
-//     {
-//         std::cout << i + 1 << "Аккоррд" << ": ";
-//         if(melody[i] & C) std::cout << "C ";
-//     }
-    
-//     return 0;
-// }
+        //     // if (noteImput >= 1 && noteImput <= 7)
+        //     // {
+        //     //     melody[i] |= 1 << (noteInput -1); // Установка битовой маски для ноты
+        //     // }
+        //     // else
+        //     // {
+        //     //     std::cout << "Неверная нота. Введите число от 1 до 7" << std::endl;
+        //     //     j--; // Повторяем ввод для данной позиции
+        //     // }
+        // }
