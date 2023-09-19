c6c8d09696f107c569bf0aac0a7652750e288eab
tokaretin <tokaretin@gmail.com>
2023-09-19 11:25:19 +0200

    task

--- Skillbox/18-task-1.cpp
+++ Skillbox/18-task-1.cpp
@@ -39,54 +39,56 @@ for(int i = 0; i < 4; ++i)
 #include <vector>
 
 // Функция swapvec для обмена значениями между вектором a и массивом b
-void swapvec(std::vector<int> &a, int *b, int size)
+void swapvec(std::vector<int> &vec, int *arr, int sizeArr)
 {
-   for (int i = 0; i < size; i++)
+   for (int i = 0; i < sizeArr; i++)
    {
-      int temp = a[i];
-      a[i] = b[i];
-      b[i] = temp;
+      int temp = vec[i];
+      vec[i] = arr[i];
+      arr[i] = temp;
    }
 }
 
 int main()
 {
-   std::cout << "Объявление вектора a и массива b и инициализация начальными значениями." << std::endl;
-   std::vector<int> a = {1, 2, 3, 4};
-   int b[] = {11, 22, 33, 44};
+   //Объявление вектора vec и массива arr и инициализация начальными значениями.
+   std::cout << "Declaring vector a and array b and initializing the initial values." << std::endl;
+   std::vector<int> vec = {1, 2, 3, 4};
+   int arr[] = {11, 22, 33, 44};
 
-   std::cout << "вектор a = ";
-   for (int i = 0; i < a.size(); i++)
+   std::cout << "vec = ";
+   for (int i = 0; i < vec.size(); i++)
    {
-      std::cout << a[i] << " ";
+      std::cout << vec[i] << " ";
    }
 
    std::cout << std::endl;
 
-   std::cout << "массив b = ";
-   for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++)
+   std::cout << "arr = ";
+   for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
    {
-      std::cout << b[i] << " ";
+      std::cout << arr[i] << " ";
    }
 
    std::cout << std::endl;
    std::cout << std::endl;
+   
+   //Функция swapvec для обмена значениями между вектором vec и массивом arr
+   std::cout << "Swapvec function for exchange of values between vector vec and array arr" << std::endl;
+   swapvec(vec, arr, sizeof(arr) / sizeof(arr[0]));
 
-   std::cout << "Функция swapvec для обмена значениями между вектором a и массивом b" << std::endl;
-   swapvec(a, b, sizeof(b) / sizeof(b[0]));
-
-   std::cout << "вектор a = ";
-   for (int i = 0; i < a.size(); i++)
+   std::cout << "vec = ";
+   for (int i = 0; i < vec.size(); i++)
    {
-      std::cout << a[i] << " ";
+      std::cout << vec[i] << " ";
    }
 
    std::cout << std::endl;
 
-   std::cout << "массив b = ";
-   for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++)
+   std::cout << "arr = ";
+   for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
    {
-      std::cout << b[i] << " ";
+      std::cout << arr[i] << " ";
    }
 
    return 0;
