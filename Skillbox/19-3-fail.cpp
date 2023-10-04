58d9550c94058eee969bc17d2f4bc97065aac4ed
tokaretin <tokaretin@gmail.com>
2023-10-04 18:31:08 +0200

    work

--- Skillbox/19-3-fail.cpp
+++ Skillbox/19-3-fail.cpp
@@ -3,18 +3,51 @@
 
 int main()
 {
+    /*
     char buffer[20]; // 20 байт
 
-    std::ifstream bank;// "C:\\cpp\\tutorial\\bank.txt"
+    std::ifstream bank; // "C:\\cpp\\tutorial\\bank.txt"
     bank.open("C:\\cpp\\tutorial\\bank.txt", std::ios::binary);
-    bank.read(buffer, sizeof(buffer));
+    bank.read(buffer, sizeof(buffer)); // читает первые 20 байт
+     bank.read(buffer, sizeof(buffer)); // читает вторые 20 байт
+     bank.read(buffer, sizeof(buffer)); // читает третьи 20 байт
 
-    // for (int i = 0; i < 20; i++)
+    // for (int i = 0; i < 20; i++) // в этом цикле мы читаем все 20 байт
     // {
     //     std::cout << buffer[i];
-    // }   
+    // }
 
+    buffer[19] = 0; // хдесь, когда мы присваеваем 0, то читаем 19 байт. 1 байт теряется
+    std::cout << buffer << std::endl;
+    bank.close();
+    */
+
+
+
+    // двигаться с любого места
+   /*
+    char buffer[20];
+
+    std::ifstream bank;
+    bank.open("C:\\cpp\\tutorial\\bank.txt", std::ios::binary);
+    bank.seekg(0); // двигаться с любого места
+    // bank.seekg(40); // двигаться с любого места
+    // bank.seekg(60); // двигаться с любого места
+    bank.read(buffer, sizeof(buffer));
     buffer[19] = 0;
     std::cout << buffer << std::endl;
-     bank.close();
+    bank.close();
+    */
+
+
+    // Указать на любое место курсор
+    char buffer[20];
+
+    std::ifstream bank;
+    bank.open("C:\\cpp\\tutorial\\bank.txt", std::ios::binary);
+    bank.read(buffer, sizeof(buffer));
+    std::cout << bank.tellg() << std::endl;
+
+
+    bank.close();
 }
--- Skillbox/tempCodeRunnerFile.cpp
+++ Skillbox/tempCodeRunnerFile.cpp
@@ -1,3 +1,15 @@
-  // note << num << str;
-    // note << num;
-    // std::cout << num << " " << str << std::endl;
+char buffer[20]; // 20 байт
+
+    // std::ifstream bank; // "C:\\cpp\\tutorial\\bank.txt"
+    // bank.open("C:\\cpp\\tutorial\\bank.txt", std::ios::binary);
+    // bank.read(buffer, sizeof(buffer)); // читает первые 20 байт
+    //  bank.read(buffer, sizeof(buffer)); // читает вторые 20 байт
+    //  bank.read(buffer, sizeof(buffer)); // читает третьи 20 байт
+    
+    // // for (int i = 0; i < 20; i++) // в этом цикле мы читаем все 20 байт
+    // // {
+    // //     std::cout << buffer[i];
+    // // }
+
+    // buffer[19] = 0; // хдесь, когда мы присваеваем 0, то читаем 19 байт. 1 байт теряется
+    // std::cout << buffer << std::endl;
